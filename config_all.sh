715abe1342ac7f84f8c7f293c06629ea16c479d4
Soto-Jnthan <67758743+Soto-Jnthan@users.noreply.github.com>
2023-02-21 17:11:41 +0100

    Add files via upload

--- config_all.sh
+++ config_all.sh
@@ -14,26 +14,34 @@ echo $password | sudo -S apt autoremove -y
 echo $password | sudo -S apt install htop -y
 
 # install necessary dependencies
-echo $password | sudo -S apt-get install -y autoconf bc build-essential g++-8 gcc-8 clang-8 lld-8 gettext-base gfortran gfortran-8 llvm-8
-echo $password | sudo -S apt-get install -y libfreetype6-dev libhdf5-dev hdf5-tools zip libjpeg8-dev liblapack-dev libblas-dev libhdf5-serial-dev
-echo $password | sudo -S apt-get install -y iputils-ping libbz2-dev libc++-dev libcgal-dev libffi-dev libjpeg-dev liblzma-dev libncurses5-dev
-echo $password | sudo -S apt-get install -y libncursesw5-dev libpng-dev libreadline-dev libssl-dev libsqlite3-dev libxml2-dev libxslt-dev
-echo $password | sudo -S apt-get install -y locales moreutils openssl rsync scons libopenblas-dev libomp5 libomp-dev libopenmpi-dev cmake zlib1g-dev
-echo $password | sudo -S apt-get install -y python3-dev python-openssl python3-pip virtualenv 
+echo $password | sudo -S apt-get install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev libopenblas-base liblapack-dev libblas-dev 
+echo $password | sudo -S apt-get install -y libatlas-base-dev gfortran libfreetype6-dev build-essential llvm-8 libomp5 libomp-dev libopenmpi-dev
+echo $password | sudo -S apt install -y python3-pip python3-dev python3-smbus cmake
+echo $password | sudo -S pip3 install -U pip testresources setuptools
+echo $password | sudo -S pip3 install flask
+echo $password | sudo -S pip3 install -U numpy==1.19.4
+echo $password | sudo -S pip3 install pillow==7.1.2
+echo $password | sudo -S pip3 install matplotlib==3.3.4
+echo $password | sudo -S pip3 install pandas==1.1.5
+echo $password | sudo -S pip3 install scipy==1.5.3
+echo $password | sudo -S pip3 install cython
+echo $password | sudo -S ln -s /usr/include/locale.h /usr/include/xlocale.h
+echo $password | sudo -S pip3 install -U future mock keras_preprocessing keras_applications gast==0.2.1 protobuf pybind11 pkgconfig packaging
+echo $password | sudo -S pip3 install -U absl-py py-cpuinfo psutil six requests termcolor pyzmq aiohttp
+echo $password | sudo -S apt-get install -y virtualenv
 
 # install traitlets (master)
 python3 -m pip install traitlets
 
 # install jupyter lab
 echo $password | sudo -S apt install -y nodejs npm
-python3 -m pip install -U jupyter jupyterlab
-python3 -m notebook --generate-config
+echo $password | sudo -S pip3 install -U jupyter jupyterlab
+jupyter lab --generate-config
 
 # set jupyter password
 python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
 
 # set paths
-echo $password | sudo -S ln -s /usr/include/locale.h /usr/include/xlocale.h
 export "LD_LIBRARY_PATH=/usr/lib/llvm-8/lib:$LD_LIBRARY_PATH"
 if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
   echo "** Add CUDA stuffs into ~/.bashrc"
@@ -45,9 +53,6 @@ fi
 # create new environment with stable-baselines2
 python3 -m virtualenv -p python3 ~/.virtualenvs/sb2 --system-site-packages
 source ~/.virtualenvs/sb2/bin/activate
-pip install -U pip testresources setuptools==65.5.0
-pip install -U numpy==1.21.1 future==0.18.2 mock==3.0.5 keras_preprocessing==1.1.2 keras_applications==1.0.8 gast==0.4.0
-pip install -U protobuf pybind11 cython pkgconfig packaging h5py==3.6.0
 pip install -U --extra-index-url https://developer.download.nvidia.com/compute/redist/jp/v461 tensorflow==1.15.5+nv22.1
 pip install stable-baselines
 python3 -m ipykernel install --user --name=sb2
@@ -57,9 +62,6 @@ deactivate
 python3 -m virtualenv -p python3 ~/.virtualenvs/sb3 --system-site-packages
 source ~/.virtualenvs/sb3/bin/activate
 wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-pip install --upgrade pip
-pip install aiohttp numpy=='1.19.4' scipy=='1.5.3'
-pip install --upgrade protobuf
 pip install -U torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 pip install -U stable-baselines3==1.3.0
 rm torch-1.10.0-cp36-cp36m-linux_aarch64.whl
@@ -91,12 +93,15 @@ echo $password | sudo -S bash -c "echo \"Exec=jupyter lab --ip=$jetsonip --no-br
 echo $password | sudo -S echo "alias sb2='source ~/.virtualenvs/sb2/bin/activate'" >> /home/$USER/.bashrc
 echo $password | sudo -S echo "alias sb3='source ~/.virtualenvs/sb3/bin/activate'" >> /home/$USER/.bashrc
 
-# setup serial communication
-python3 -m pip install pyserial
+# setup low-level controllers
+echo $password | sudo -S -H pip3 install -U jetson-stats
+echo $password | sudo -S pip3 install -U Jetson.GPIO
+echo $password | sudo -S pip3 install pyserial
 echo $password | sudo -S adduser $USER dialout
 
 # reboot the system
 echo $password | sudo -S apt update
 echo $password | sudo -S apt clean -y
 echo $password | sudo -S apt autoremove -y
+echo $password | sudo -S systemctl restart jetson_stats.service
 echo $password | sudo -S reboot

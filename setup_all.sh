d4bc36dc3d34821470ec26583ca5618091f32912
Soto-Jnthan <67758743+Soto-Jnthan@users.noreply.github.com>
2023-02-22 18:11:32 +0100

    Add files via upload

--- setup_all.sh
+++ setup_all.sh
@@ -8,7 +8,7 @@ SCRIPTPATH="$(realpath $0)"
 
 setup_step1()
 {
-    sudo apt -y update && sudo apt -y upgrade
+	sudo apt -y update && sudo apt -y upgrade
 	sudo apt remove --purge libreoffice* -y
 	sudo apt autoremove -y
 	sudo apt install -y htop python3-pip python-pip
@@ -25,59 +25,59 @@ setup_step1()
 setup_step2()
 {
 	setup_gpio_and_comms
-    cd ~
-    mkdir -p install
-    cd install
-    wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh .
-    chmod a+x Miniforge3-Linux-aarch64.sh
-    ./Miniforge3-Linux-aarch64.sh
+	cd ~
+	mkdir -p install
+	cd install
+	wget https://github.com/conda-forge/miniforge/releases/latest/download/Miniforge3-Linux-aarch64.sh .
+	chmod a+x Miniforge3-Linux-aarch64.sh
+	./Miniforge3-Linux-aarch64.sh
 }
 
 setup_step3()
 {
-    conda config --set auto_activate_base false
-    sudo apt install -y python3-h5py libhdf5-serial-dev hdf5-tools libpng-dev libfreetype6-dev
-    conda create -y -n sb3 python=3.6
-    conda install -n sb3 -y matplotlib pandas numpy pillow scipy tqdm scikit-image scikit-learn seaborn cython h5py jupyter ipywidgets -c conda-forge
+	conda config --set auto_activate_base false
+	sudo apt install -y python3-h5py libhdf5-serial-dev hdf5-tools libpng-dev libfreetype6-dev
+	conda create -y -n sb3 python=3.6
+	conda install -n sb3 -y matplotlib pandas numpy pillow scipy tqdm scikit-image scikit-learn seaborn cython h5py jupyter ipywidgets -c conda-forge
 	install_SB3
 	setup_jupyterlab
 	make_swapfile
 	switch_to_lubuntu
-    sudo -S apt update
-    sudo -S apt clean -y
-    sudo -S apt autoremove -y
+	sudo -S apt update
+	sudo -S apt clean -y
+	sudo -S apt autoremove -y
 }
 
 setup_install_folder()
 {
-    if [ ! -d ~/install ] 
-    then
-        mkdir -p ~/install 
-    fi
-    cd ~/install
-    eval "$(conda shell.bash hook)"
-    conda activate sb3
+	if [ ! -d ~/install ]
+	then
+		mkdir -p ~/install
+	fi
+	cd ~/install
+	eval "$(conda shell.bash hook)"
+	conda activate sb3
 }
 
 teardown_install_folder()
 {
-    conda deactivate
+	conda deactivate
 }
 
 install_SB3()
 {
-    setup_install_folder
-    wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-    sudo apt-get install -y libopenblas-base libopenmpi-dev
-    pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+	setup_install_folder
+	wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+	sudo apt-get install -y libopenblas-base libopenmpi-dev
+	pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
 	conda install -n sb3 -y stable-baselines3==1.3.0 -c conda-forge
-    teardown_install_folder
+	teardown_install_folder
 }
 
 setup_jupyterlab()
 {
-    setup_install_folder
-    conda install -y jupyterlab
+	setup_install_folder
+	conda install -y jupyterlab
 	jupyter lab --generate-config
 	python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
 	jetsonip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(192.([0-9]*\.){2}[0-9]*).*/\2/p'`
@@ -85,7 +85,7 @@ setup_jupyterlab()
 	sudo bash -c "echo \"Name=jupyterlab\" >> /etc/xdg/autostart/jupyterlab.desktop"
 	sudo bash -c "echo \"Exec=jupyter lab --ip=$jetsonip --no-browser --allow-root\" >> /etc/xdg/autostart/jupyterlab.desktop"
 	sudo echo "alias sb3='conda activate sb3'" >> /home/$USER/.bashrc
-    teardown_install_folder
+	teardown_install_folder
 }
 
 make_swapfile()
@@ -115,67 +115,67 @@ setup_gpio_and_comms()
 
 write_boot_script_step2()
 {
-    echo "$SCRIPTPATH -2 $OPTIONS" >> ~/.bashrc
+	echo "$SCRIPTPATH -2 $OPTIONS" >> ~/.bashrc
 }
 
 write_boot_script_step3()
 {
-    echo "$SCRIPTPATH -3 $OPTIONS" >> ~/.bashrc
+	echo "$SCRIPTPATH -3 $OPTIONS" >> ~/.bashrc
 }
 
 remove_boot_script()
 {
-    sed -i '$ d' ~/.bashrc
+	sed -i '$ d' ~/.bashrc
 }
 
 main()
 {
-    echo " [  Jetson Nano Setup v0.1  ]"
-	
-    INSTALL_STEP=0
-    while true; do
-        case "$1" in 
-            -1) 
-                if [ "$INSTALL_STEP" -le "1" ]
-                then 
-                    INSTALL_STEP=1 
-                fi
-                ;;
-            -2) 
-                if [ "$INSTALL_STEP" -le "2" ]
-                then 
-                    INSTALL_STEP=2 
-                fi
-                ;;
-            -3) 
-                if [ "$INSTALL_STEP" -le "3" ]
-                then 
-                    INSTALL_STEP=3 
-                fi
-                ;;
-            -- ) break ;;
-        esac
-        shift
-    done
+	echo " [  Jetson Nano Setup v0.1  ]"
 
-    if [ "$INSTALL_STEP" -eq "0" ] ; then
-        INSTALL_STEP=1
-    fi
+	INSTALL_STEP=0
+	while true; do
+		case "$1" in
+			-1)
+				if [ "$INSTALL_STEP" -le "1" ]
+				then
+					INSTALL_STEP=1
+				fi
+				;;
+			-2)
+				if [ "$INSTALL_STEP" -le "2" ]
+				then
+					INSTALL_STEP=2
+				fi
+				;;
+			-3)
+				if [ "$INSTALL_STEP" -le "3" ]
+				then
+					INSTALL_STEP=3
+				fi
+				;;
+			-- ) break ;;
+		esac
+		shift
+	done
 
-    case $INSTALL_STEP in
-        1)
-            echo "Setting up step 1.."
-            setup_step1 && write_boot_script_step2 && sudo reboot
-            ;;
-        2) 
-            echo "Setting up step 2.."
-            remove_boot_script && setup_step2 && write_boot_script_step3 && sudo reboot
-            ;;
-        3)
-            echo "Setting up step 3.."
-            remove_boot_script && setup_step3
-            ;;
-    esac
+	if [ "$INSTALL_STEP" -eq "0" ] ; then
+		INSTALL_STEP=1
+	fi
+
+	case $INSTALL_STEP in
+	1)
+		echo "Setting up step 1.."
+		setup_step1 && write_boot_script_step2 && sudo reboot
+		;;
+	2)
+		echo "Setting up step 2.."
+		remove_boot_script && setup_step2 && write_boot_script_step3 && sudo reboot
+		;;
+	3)
+		echo "Setting up step 3.."
+		remove_boot_script && setup_step3
+		;;
+	esac
 }
 
 main $@

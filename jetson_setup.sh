826eb3022843ece4901776eafcaf2a08fef17c03
Soto-Jnthan <67758743+Soto-Jnthan@users.noreply.github.com>
2023-02-23 20:59:07 +0100

    Add files via upload

--- jetson_setup.sh
+++ jetson_setup.sh
@@ -9,7 +9,9 @@ setup_step1()
     sudo apt -y update && sudo apt -y upgrade
     sudo apt remove --purge libreoffice* -y
     sudo -S apt clean -y && sudo -S apt autoremove -y
-    sudo apt install -y nano curl htop python3-pip python-pip
+    sudo apt install -y libhdf5-serial-dev hdf5-tools libhdf5-dev zlib1g-dev zip libjpeg8-dev libopenblas-base liblapack-dev  
+    sudo apt install -y libatlas-base-dev gfortran libfreetype6-dev build-essential libopenmpi-dev libblas-dev libpng-dev
+    sudo apt install -y nano htop python3-pip python3-dev python3-smbus cmake
     sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
     if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then
         echo "** Add CUDA stuffs into ~/.bashrc"
@@ -22,74 +24,50 @@ setup_step1()
 
 setup_step2()
 {
-    setup_gpio_and_comms
-    cd ~
-    mkdir -p install
-    cd install 
-    wget https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-pypy3-Linux-aarch64.sh .
-    chmod a+x Mambaforge-pypy3-Linux-aarch64.sh
-    ./Mambaforge-pypy3-Linux-aarch64.sh
-}
-
-setup_gpio_and_comms()
-{
+    sudo pip3 install -U pip testresources setuptools
+    sudo pip3 install -U flask
+    sudo pip3 install -U numpy==1.19.4 pillow==8.4.0 matplotlib==3.3.4 pandas==1.1.5 scipy==1.5.3 cython
+    sudo pip3 install -U h5py scikit-learn==0.22.0 seaborn==0.10.1 
+    sudo pip3 install -U traitlets
     sudo -H pip3 install -U jetson-stats==3.1.4
-    sudo systemctl restart jetson_stats.service
     sudo pip3 install -U Jetson.GPIO pyserial
+    sudo apt install -y virtualenv
     sudo adduser $USER dialout
+	sudo systemctl restart jetson_stats.service
 }
 
 setup_step3()
 {
-    mamba config --set auto_activate_base false
-    sudo apt install -y python3-h5py libhdf5-serial-dev hdf5-tools libpng-dev libfreetype6-dev
-    mamba create -y -n sb3 python=3.6
-    mamba install -n sb3 -y matplotlib pandas numpy pillow scipy tqdm scikit-image scikit-learn seaborn cython h5py jupyter ipywidgets -c conda-forge
-    install_SB3
+    sudo apt install -y nodejs npm
+    sudo pip3 install -U jupyter jupyterlab
     setup_jupyterlab
+    install_SB3
     make_swapfile
     switch_to_lubuntu
     sudo -S apt clean -y && sudo -S apt autoremove -y
 }
 
-setup_install_folder()
-{
-    if [ ! -d ~/install ]
-    then
-        mkdir -p ~/install
-    fi
-    cd ~/install
-    eval "$(conda shell.bash hook)"
-    mamba activate sb3
-}
-
-teardown_install_folder()
-{
-    mamba deactivate
-}
-
 install_SB3()
 {
-    setup_install_folder
+    python3 -m virtualenv -p python3 ~/.virtualenvs/sb3 --system-site-packages
+    source ~/.virtualenvs/sb3/bin/activate
     wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-    sudo apt-get install -y libopenblas-base libopenmpi-dev
-    pip install torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-    mamba install -n sb3 -y stable-baselines3==1.3.0 -c conda-forge
-    teardown_install_folder
+    pip install -U torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+    pip install -U stable-baselines3==1.3.0
+    rm torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+    python3 -m ipykernel install --user --name=sb3
+    deactivate
+    sudo echo "alias sb3='source ~/.virtualenvs/sb3/bin/activate'" >> /home/$USER/.bashrc
 }
 
 setup_jupyterlab()
 {
-    setup_install_folder
-    mamba install -y jupyterlab
     jupyter lab --generate-config
     python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
     jetsonip=`ifconfig | sed -En 's/127.0.0.1//;s/.*inet (addr:)?(192.([0-9]*\.){2}[0-9]*).*/\2/p'`
     sudo bash -c "echo \"[Desktop Entry]\" >> /etc/xdg/autostart/jupyterlab.desktop"
     sudo bash -c "echo \"Name=jupyterlab\" >> /etc/xdg/autostart/jupyterlab.desktop"
     sudo bash -c "echo \"Exec=jupyter lab --ip=$jetsonip --no-browser --allow-root\" >> /etc/xdg/autostart/jupyterlab.desktop"
-    sudo echo "alias sb3='mamba activate sb3'" >> /home/$USER/.bashrc
-    teardown_install_folder
 }
 
 make_swapfile()

ab37bd6c552996bcdca858d199ce948263bfab0e
Soto-Jnthan <67758743+Soto-Jnthan@users.noreply.github.com>
2023-02-26 18:25:45 +0100

    Add files via upload

--- jetson_setup.sh
+++ jetson_setup.sh
@@ -8,7 +8,7 @@ setup_step1()
 {
     sudo apt -y update && sudo apt -y upgrade
     sudo apt remove --purge libreoffice* nodejs* -y
-    sudo apt install -y nano htop python3-pip build-essential libzmq3-dev libffi-dev libssl1.0-dev npm virtualenv
+    sudo apt install -y nano htop dkms python3-pip libzmq3-dev libffi-dev libssl1.0-dev npm virtualenv
     sudo apt install -y libhdf5-serial-dev hdf5-tools libpng-dev libfreetype6-dev libblas-dev libopenblas-base libopenmpi-dev
     sudo ln -s /usr/include/locale.h /usr/include/xlocale.h
     if ! grep 'cuda/bin' ${HOME}/.bashrc > /dev/null ; then 
@@ -26,17 +26,50 @@ setup_step2()
     sudo npm cache clean -f
     sudo npm install -g n
     sudo n 16
-    python3 -m pip install --user -U pip testresources setuptools 
-    python3 -m pip install --user install flask 
-    python3 -m pip install --user install -U numpy==1.19.4 
-    python3 -m pip install --user install -U scipy==1.5.3
-    python3 -m pip install --user install -U matplotlib 
-    python3 -m pip install --user install -U Cython packaging Jetson.GPIO pyserial
-    python3 -m pip install --user install jupyterlab
+	create_SB3_env
     sudo -H python3 -m pip install -U jetson-stats==3.1.4
     sudo adduser $USER dialout
-    sudo systemctl restart jetson_stats.service	
+    sudo systemctl restart jetson_stats.service
+    install_wifi_drivers	
     install_fan_drivers
+}
+
+create_SB3_env()
+{
+    python3 -m virtualenv -p python3 ~/.virtualenvs/sb3
+    echo "alias sb3='source ~/.virtualenvs/sb3/bin/activate'" >> ${HOME}/.bashrc
+    source ~/.virtualenvs/sb3/bin/activate
+    wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+    python -m pip install -U pip testresources setuptools 
+    python -m pip install flask 
+    python -m pip install -U numpy==1.19.4 
+    python -m pip install -U scipy==1.5.3
+    python -m pip install -U matplotlib 
+    python -m pip install -U Cython packaging Jetson.GPIO pyserial
+    python -m pip install jupyterlab
+    python -m pip -U torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+    python -m pip -U stable-baselines3==1.3.0 tensorboard
+    rm torch-1.10.0-cp36-cp36m-linux_aarch64.whl
+    python -m ipykernel install --name=sb3
+    deactivate
+}
+
+install_wifi_drivers()
+{
+    git clone https://github.com/jeremyb31/rtl8812au-1
+    cd rtl8812au-1
+    sudo ./dkms-install.sh
+    cd ..
+    sudo rm -r rtl8812au-1
+}
+
+install_fan_drivers()
+{
+    git clone https://github.com/Pyrestone/jetson-fan-ctl
+    cd jetson-fan-ctl
+    sudo ./install.sh
+    cd ..
+    sudo rm -r jetson-fan-ctl
 }
 
 setup_step3()
@@ -50,29 +83,17 @@ setup_step3()
 
 setup_jupyterlab()
 {
+    sb3
     jupyter labextension install @jupyter-widgets/jupyterlab-manager
     jupyter lab --generate-config
-    python3 -c "from notebook.auth.security import set_password; set_password('$password', '$HOME/.jupyter/jupyter_notebook_config.json')"
-    sudo bash -c "echo \"[Desktop Entry]\" >> /etc/xdg/autostart/jupyterlab.desktop"
-    sudo bash -c "echo \"Name=jupyterlab\" >> /etc/xdg/autostart/jupyterlab.desktop"
-    sudo bash -c "echo \"Exec=jupyter lab --ip=$(ip -o route get 8.8.8.8 | grep -oP '(?<=src )\S+') --no-browser --allow-root\" >> /etc/xdg/autostart/jupyterlab.desktop"
+    echo "[Desktop Entry]" | sudo tee -a -i /etc/xdg/autostart/jupyterlab.desktop
+    echo "Name=jupyterlab" | sudo tee -a -i /etc/xdg/autostart/jupyterlab.desktop
+    echo 'Exec=bash -c '"'"'sb3 && jupyter lab --ip=$(ip -o route get 8.8.8.8 | grep -oP "(?<=src )\S+") --no-browser --allow-root'"'"'' | sudo tee -a -i /etc/xdg/autostart/jupyterlab.desktop
     echo >> ${HOME}/.bashrc
     echo "if ! jupyter lab list | grep -q 'http' ; then" >> ${HOME}/.bashrc
-    echo "	jupyter lab --ip=$(ip -o route get 8.8.8.8 | grep -oP '(?<=src )\S+') --no-browser --allow-root &" >> ${HOME}/.bashrc
+    echo '	sb3 && jupyter lab --ip=$(ip -o route get 8.8.8.8 | grep -oP "(?<=src )\S+") --no-browser --allow-root &' >> ${HOME}/.bashrc
     echo "fi" >> ${HOME}/.bashrc
-}
-
-install_SB3()
-{
-    python3 -m virtualenv -p python3 ~/.virtualenvs/sb3 --system-site-packages
-    source ~/.virtualenvs/sb3/bin/activate
-    wget https://nvidia.box.com/shared/static/fjtbno0vpo676a25cgvuqc1wty0fkkg6.whl -O torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-    pip install -U torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-    pip install -U stable-baselines3==1.3.0 tensorboard
-    rm torch-1.10.0-cp36-cp36m-linux_aarch64.whl
-    python3 -m ipykernel install --user --name=sb3
     deactivate
-    echo "alias sb3='source ~/.virtualenvs/sb3/bin/activate'" >> ${HOME}/.bashrc
 }
 
 make_swapfile()
@@ -82,15 +103,6 @@ make_swapfile()
     sudo mkswap /var/swapfile
     sudo swapon /var/swapfile
     sudo bash -c 'echo "/var/swapfile swap swap defaults 0 0" >> /etc/fstab'
-}
-
-install_fan_drivers()
-{
-    git clone https://github.com/Pyrestone/jetson-fan-ctl
-    cd jetson-fan-ctl
-    sudo ./install.sh
-    cd ..
-    sudo rm -r jetson-fan-ctl
 }
 
 switch_to_lubuntu()
